6d0e88d791ea1dfd59bf7a966984ba8e36fb8bbe
Paul Frazee <pfrazee@gmail.com>
2025-03-27 15:59:02 -0700

    Add post: lexicon-guidance

---
+++ components/Callout.tsx
@@ -0,0 +1,9 @@
+const Callout = ({ children }) => (
+  <div className="md px-4 py-2">
+    <div className="rounded-md bg-gray-50 px-6 py-1 dark:border-gray-700 dark:bg-gray-900">
+      {children}
+    </div>
+  </div>
+)
+
+export default Callout
